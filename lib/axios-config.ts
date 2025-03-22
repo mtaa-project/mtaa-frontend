ad20ea6a914b49f1e419445b94231f3f91dd562c
SVBSH <getenv@protonmail.com>
2025-03-22 15:45:58 +0100

    Add camelcase-keys and snakecase-keys for data transformation in axios requests. Update axios configuration to convert request data to snake_case and response data to camelCase for improved API integration.

--- lib/axios-config.ts
+++ lib/axios-config.ts
@@ -1,4 +1,6 @@
 import axios from "axios"
+import camelcaseKeys from "camelcase-keys"
+import snakecaseKeys from "snakecase-keys"
 
 import { auth } from "@/firebase-config"
 
@@ -12,7 +14,17 @@ api.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
+    if (config.data && config.method !== "get") {
+      config.data = snakecaseKeys(config.data, { deep: true })
+    }
     return config
   },
   (error) => Promise.reject(error)
 )
+
+api.interceptors.request.use(async (response) => {
+  if (response.data) {
+    response.data = camelcaseKeys(response.data, { deep: true })
+  }
+  return response
+})
