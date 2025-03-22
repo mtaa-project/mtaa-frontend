4270ec7ed1ad51a5cb8d0540a00d012d46e47c19
SVBSH <getenv@protonmail.com>
2025-03-22 21:35:55 +0100

    Enhance Home component to fetch and display user data from API. Refactor sign-out button to use react-native-paper for improved styling. Introduce state management for users and update useEffect for data retrieval.

--- src/app/(auth)/home.tsx
+++ src/app/(auth)/home.tsx
@@ -1,20 +1,39 @@
-import { useEffect } from "react"
-import { Button, Text, View } from "react-native"
+import { useEffect, useState } from "react"
+import { View } from "react-native"
+import { Button, Text } from "react-native-paper"
 
 import { auth } from "@/firebase-config"
+import { api } from "@/src/lib/axios-config"
 
 export default function Home() {
   const user = auth.currentUser
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
-    // foo()
+    const foo = async () => {
+      try {
+        const users = await api.get("/users")
+        if (users.data) {
+          setUsers(users.data)
+        }
+      } catch {}
+    }
+    foo()
   }, [user])
+
   return (
     <View>
       <Text>Welcome back {user?.email}</Text>
       <Text style={{ color: "white" }}>data: </Text>
 
-      <Button title="Sign out" onPress={() => auth.signOut()} />
+      <Button mode="contained" onPress={() => auth.signOut()}>
+        Sign out
+      </Button>
+
+      {users &&
+        users.map((user, index) => (
+          <Text key={index}>{JSON.stringify(user)}</Text>
+        ))}
     </View>
   )
 }
