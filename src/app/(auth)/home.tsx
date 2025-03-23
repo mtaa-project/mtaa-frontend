60545c46aa6f9bf304a962ec62c82005d7c3b6a6
SVBSH <getenv@protonmail.com>
2025-03-23 01:58:38 +0100

    Refactor Home component to replace inline Facebook authentication logic with a dedicated function for linking accounts. This improves code modularity and organization by utilizing the new linkAccountFacebook function.

--- src/app/(auth)/home.tsx
+++ src/app/(auth)/home.tsx
@@ -1,14 +1,9 @@
-import {
-  type AuthCredential,
-  FacebookAuthProvider,
-  linkWithCredential,
-} from "firebase/auth"
 import { useEffect, useState } from "react"
 import { View } from "react-native"
-import { AccessToken, LoginManager } from "react-native-fbsdk-next"
 import { Button, Text } from "react-native-paper"
 
 import { auth } from "@/firebase-config"
+import { linkAccountFacebook } from "@/src/components/auth/facebook-auth"
 import { api } from "@/src/lib/axios-config"
 
 export default function Home() {
@@ -27,50 +22,6 @@ export default function Home() {
     foo()
   }, [user])
 
-  const linkAccount = async (credential: AuthCredential) => {
-    if (auth.currentUser) {
-      try {
-        const userCredential = await linkWithCredential(
-          auth.currentUser,
-          credential
-        )
-        console.log("Your account was successfully linked:", userCredential)
-      } catch (error) {
-        console.error("Error with linking the account:", error)
-      }
-    } else {
-      console.warn("Link credential or current user not available")
-    }
-  }
-  // const setLinkCredential = useUserStore((state) => state.setLinkCredential)
-
-  const SignInWithFB = async () => {
-    try {
-      const fbResult = await LoginManager.logInWithPermissions([
-        "public_profile",
-        "email",
-      ])
-
-      if (fbResult.isCancelled) {
-        throw new Error("Something went wrong...")
-      }
-
-      const data = await AccessToken.getCurrentAccessToken()
-      if (!data) {
-        throw new Error("Something went wrong with obtaining access token...")
-      }
-
-      const credential = FacebookAuthProvider.credential(data.accessToken)
-      await linkAccount(credential)
-    } catch (error: any) {
-      // const e = error as FirebaseError
-      if (error.code === "auth/account-exists-with-different-credential") {
-        const email = error.customData.email
-        const cred = FacebookAuthProvider.credentialFromError(error)
-      }
-    }
-  }
-
   return (
     <View>
       <Text>Welcome back {user?.email}</Text>
@@ -79,7 +30,7 @@ export default function Home() {
       <Button mode="contained" onPress={() => auth.signOut()}>
         Sign out
       </Button>
-      <Button mode="contained" onPress={() => SignInWithFB()}>
+      <Button mode="contained" onPress={() => linkAccountFacebook()}>
         Link Account
       </Button>
 
