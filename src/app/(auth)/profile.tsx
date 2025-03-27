c65b90fcae9f805e4d7f56d588ed7d89f899e37d
SVBSH <getenv@protonmail.com>
2025-03-27 22:31:51 +0100

    Enhance Profile component to support linking Google accounts alongside Facebook. Introduce state management for Google account status and refactor account linking logic to handle both account types. Update button rendering for improved user feedback on account linking status.

--- src/app/(auth)/profile.tsx
+++ src/app/(auth)/profile.tsx
@@ -4,10 +4,16 @@ import { Button, Dialog, Paragraph, Portal, Text } from "react-native-paper"
 
 import { auth } from "@/firebase-config"
 import { linkAccountFacebook } from "@/src/components/auth/facebook-auth"
+import { useGoogleAuth } from "@/src/components/auth/google-auth"
+
+type AccountType = "Facebook" | "Google"
 
 export default function Profile() {
   const user = auth.currentUser
   const [hasFacebook, setHasFacebook] = useState<boolean>(false)
+  const [hasGoogleLinked, setHasGoogleLinked] = useState<boolean>(false)
+
+  const { response, googleSignIn } = useGoogleAuth({ linkAccount: true })
 
   const [linkingAccountError, setLinkingAccountError] = useState<string | null>(
     null
@@ -18,14 +24,19 @@ export default function Profile() {
     if (user) {
       const providers = user.providerData.map((provider) => provider.providerId)
       setHasFacebook(providers.includes("facebook.com"))
+      setHasGoogleLinked(providers.includes("google.com"))
     }
   }, [user])
 
-  const handleLinkFacebookAccount = async () => {
+  const handleLinkAccount = async (accountType: AccountType) => {
     try {
-      await linkAccountFacebook()
-      setHasFacebook(true)
-      console.log("dobre")
+      if (accountType === "Facebook") {
+        await linkAccountFacebook()
+        setHasFacebook(true)
+      } else if (accountType === "Google") {
+        await googleSignIn()
+        setHasGoogleLinked(true)
+      }
     } catch (error: any) {
       console.log("zle")
       setLinkingAccountError(error.message)
@@ -40,9 +51,17 @@ export default function Profile() {
         icon={"facebook"}
         mode="contained"
         disabled={hasFacebook}
-        onPress={handleLinkFacebookAccount}
+        onPress={() => handleLinkAccount("Facebook")}
       >
         {hasFacebook ? "Linked" : "Link Facebook Account"}
+      </Button>
+      <Button
+        icon={"google"}
+        mode="contained"
+        disabled={hasGoogleLinked}
+        onPress={() => handleLinkAccount("Google")}
+      >
+        {hasGoogleLinked ? "Linked" : "Link Google Account"}
       </Button>
       <Button mode="contained" onPress={() => auth.signOut()}>
         Sign out
