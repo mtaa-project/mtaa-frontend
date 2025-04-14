f3b67f537c356eb5018d09f6b53620136856a434
SVBSH <getenv@protonmail.com>
2025-04-14 02:08:25 +0200

    Update dependencies and implement push notification functionality in Home component
    - Bump Expo and React Native dependencies to latest versions for improved performance and security.
    - Integrate Expo Notifications in Home component to handle push notifications.
    - Add functions for registering devices for push notifications and sending notifications.
    - Set up notification handlers and listeners to manage incoming notifications and responses.
    - Enhance user experience by providing feedback on push notification registration status.

--- src/app/(auth)/home.tsx
+++ src/app/(auth)/home.tsx
@@ -1,9 +1,85 @@
-import { useEffect, useState } from "react"
-import { StyleSheet, View } from "react-native"
+import * as Device from "expo-device"
+import * as Notifications from "expo-notifications"
+import { useEffect, useRef, useState } from "react"
+import { Platform, StyleSheet, View } from "react-native"
 import { type MD3Theme, Text, useTheme } from "react-native-paper"
 
 import { auth } from "@/firebase-config"
 import { api } from "@/src/lib/axios-config"
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+})
+
+async function sendPushNotification(expoPushToken: string) {
+  const message = {
+    to: expoPushToken,
+    sound: "default",
+    title: "Original Title",
+    body: "And here is the body!",
+    data: { someData: "goes here" },
+  }
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  })
+}
+
+function handleRegistrationError(errorMessage: string) {
+  alert(errorMessage)
+  throw new Error(errorMessage)
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    })
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
+    let finalStatus = existingStatus
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync()
+      finalStatus = status
+    }
+    if (finalStatus !== "granted") {
+      handleRegistrationError(
+        "Permission not granted to get push token for push notification!"
+      )
+      return
+    }
+    const projectId = "mtaa-project-5235a"
+    if (!projectId) {
+      handleRegistrationError("Project ID not found")
+    }
+    try {
+      const pushTokenString = (await Notifications.getDevicePushTokenAsync())
+        .data
+
+      console.log("PushToken: ", pushTokenString)
+      return pushTokenString
+    } catch (e: unknown) {
+      handleRegistrationError(`${e}`)
+    }
+  } else {
+    handleRegistrationError("Must use physical device for push notifications")
+  }
+}
 
 export default function Home() {
   const user = auth.currentUser
@@ -23,6 +99,38 @@ export default function Home() {
     foo()
   }, [user])
 
+  const [expoPushToken, setExpoPushToken] = useState("")
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined)
+  const notificationListener = useRef<Notifications.EventSubscription>()
+  const responseListener = useRef<Notifications.EventSubscription>()
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error: any) => setExpoPushToken(`${error}`))
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification)
+      })
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response)
+      })
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        )
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current)
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Welcome back {user?.email}</Text>
@@ -32,6 +140,7 @@ export default function Home() {
         users.map((user, index) => (
           <Text key={index}>{JSON.stringify(user)}</Text>
         ))}
+      {/* <Text>{expoPushToken}</Text> */}
     </View>
   )
 }
