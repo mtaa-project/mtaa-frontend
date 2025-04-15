f638301f362027441d3800d37424cb58dba2f09b
SVBSH <getenv@protonmail.com>
2025-04-15 18:13:28 +0200

    Implement push notification functionality and refactor Home component
    - Introduce a new notification module to handle push notifications, including registration and sending functions.
    - Refactor the Home component to utilize the new notification module, improving code organization and readability.
    - Update package.json to specify the package manager version.

--- src/app/(auth)/home.tsx
+++ src/app/(auth)/home.tsx
@@ -1,85 +1,11 @@
-import * as Device from "expo-device"
 import * as Notifications from "expo-notifications"
 import { useEffect, useRef, useState } from "react"
-import { Platform, StyleSheet, View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import { type MD3Theme, Text, useTheme } from "react-native-paper"
 
 import { auth } from "@/firebase-config"
 import { api } from "@/src/lib/axios-config"
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: true,
-  }),
-})
-
-async function sendPushNotification(expoPushToken: string) {
-  const message = {
-    to: expoPushToken,
-    sound: "default",
-    title: "Original Title",
-    body: "And here is the body!",
-    data: { someData: "goes here" },
-  }
-
-  await fetch("https://exp.host/--/api/v2/push/send", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Accept-encoding": "gzip, deflate",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(message),
-  })
-}
-
-function handleRegistrationError(errorMessage: string) {
-  alert(errorMessage)
-  throw new Error(errorMessage)
-}
-
-async function registerForPushNotificationsAsync() {
-  if (Platform.OS === "android") {
-    Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    })
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync()
-    let finalStatus = existingStatus
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync()
-      finalStatus = status
-    }
-    if (finalStatus !== "granted") {
-      handleRegistrationError(
-        "Permission not granted to get push token for push notification!"
-      )
-      return
-    }
-    const projectId = "mtaa-project-5235a"
-    if (!projectId) {
-      handleRegistrationError("Project ID not found")
-    }
-    try {
-      const pushTokenString = (await Notifications.getDevicePushTokenAsync())
-        .data
-
-      console.log("PushToken: ", pushTokenString)
-      return pushTokenString
-    } catch (e: unknown) {
-      handleRegistrationError(`${e}`)
-    }
-  } else {
-    handleRegistrationError("Must use physical device for push notifications")
-  }
-}
+import { registerForPushNotificationsAsync } from "@/src/lib/notification"
 
 export default function Home() {
   const user = auth.currentUser
@@ -107,9 +33,14 @@ export default function Home() {
   const responseListener = useRef<Notifications.EventSubscription>()
 
   useEffect(() => {
-    registerForPushNotificationsAsync()
-      .then((token) => setExpoPushToken(token ?? ""))
-      .catch((error: any) => setExpoPushToken(`${error}`))
+    ;(async () => {
+      try {
+        const token = await registerForPushNotificationsAsync()
+        setExpoPushToken(token)
+      } catch (error) {
+        setExpoPushToken(`${error}`)
+      }
+    })()
 
     notificationListener.current =
       Notifications.addNotificationReceivedListener((notification) => {
