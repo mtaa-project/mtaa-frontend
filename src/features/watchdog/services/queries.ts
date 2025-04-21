da9c2c71c16c9cab18a8dd005f3efc0af14e5db9
SVBSH <getenv@protonmail.com>
2025-04-21 02:10:53 +0200

    Refactor Watchdog components and types for improved data handling and user experience
    - Removed PriceRange type from types.ts and updated related components to streamline price handling.
    - Adjusted WatchdogModal and related components to utilize new price range structure with minPrice and maxPrice.
    - Enhanced form handling in RHFTextInput to support number parsing and improved error handling.
    - Updated API functions to accommodate changes in price range structure for better data consistency.

--- src/features/watchdog/services/queries.ts
+++ src/features/watchdog/services/queries.ts
@@ -2,6 +2,7 @@ import { apiGetMyWatchdogList, apiGetWatchdog } from "@/src/api/watchdog"
 import { useQuery } from "@tanstack/react-query"
 import { FilterSchemaType } from "../components/watchdog-modal/filter-schema"
 import { apiGetCategories } from "@/src/api/categories"
+import { OfferType } from "@/src/api/types"
 
 export const useGetWatchdogList = () => {
   return useQuery({
