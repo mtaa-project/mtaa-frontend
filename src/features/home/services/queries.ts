52ac977de2d99d5443176be3fd4bbc50a3b023f5
samuel-racak <samuel.racak@gmail.com>
2025-05-04 22:08:03 +0200

    Add user favorite listings queries: implement hooks for fetching favorite listings and paginated results

---
+++ src/features/home/services/queries.ts
@@ -0,0 +1,19 @@
+import {
+  apiGetFavoriteListings,
+  apiGetFavoriteListingsPaginated,
+} from "@/src/api/favorites"
+import { useQuery } from "@tanstack/react-query"
+
+export const useUserFavoriteListings = () => {
+  return useQuery({
+    queryKey: ["user", "favorites"],
+    queryFn: () => apiGetFavoriteListings(),
+  })
+}
+
+export const useUserFavoriteListingsPaginated = (pageParam: number) => {
+  return useQuery({
+    queryKey: ["user", "favorites", pageParam],
+    queryFn: () => apiGetFavoriteListingsPaginated(pageParam),
+  })
+}
