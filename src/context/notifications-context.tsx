2167eb7db4c53bd39b5b43c78b50b2c309b9a0ee
SVBSH <getenv@protonmail.com>
2025-05-05 00:51:17 +0200

    Refactor notification cleanup: simplify removal of notification listeners using optional chaining

--- src/context/notifications-context.tsx
+++ src/context/notifications-context.tsx
@@ -81,12 +81,8 @@ export const NotificationProvider: React.FC<NotificationProviderProps> = ({
       })
 
     return () => {
-      notificationListener.current &&
-        Notifications.removeNotificationSubscription(
-          notificationListener.current
-        )
-      responseListener.current &&
-        Notifications.removeNotificationSubscription(responseListener.current)
+      notificationListener.current?.remove()
+      responseListener.current?.remove()
     }
   }, [])
 
