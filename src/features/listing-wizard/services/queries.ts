c68a138eb259a2191f016e3e0cd8d448f670ac8c
samuel-racak <samuel.racak@gmail.com>
2025-05-04 22:04:50 +0200

    Add TODO comment to remove useListingDetails as it is in global API

--- src/features/listing-wizard/services/queries.ts
+++ src/features/listing-wizard/services/queries.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 import { apiGetListingDetails } from "./api"
 
+// TODO: remove this as it is in global API
 export const useListingDetails = (id?: number) => {
   return useQuery({
     queryKey: ["listings", "details", id],
