fc64c2f7f0778f2bbdcc1b61f1ce59721a565e0d
samuel-racak <samuel.racak@gmail.com>
2025-05-04 22:04:29 +0200

    Remove unused styles and clean up ListingDetailsScreen component

--- src/app/(auth)/(tabs)/listings/[id].tsx
+++ src/app/(auth)/(tabs)/listings/[id].tsx
@@ -1,17 +1,8 @@
 import { ListingDetail } from "@/src/features/listing-detail/listing-detail"
 import { useLocalSearchParams } from "expo-router"
-import { View, Text, StyleSheet } from "react-native"
 
 export default function ListingDetailsScreen() {
   const { id } = useLocalSearchParams()
 
   return <ListingDetail listingId={Number(id)}></ListingDetail>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-})
