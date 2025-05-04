2257d16fcbdcc9f50455ee592edaea27027a08ed
samuel-racak <samuel.racak@gmail.com>
2025-05-04 18:57:05 +0200

    Make phoneNumber optional in UserProfileBase and ApiSellerContact types

--- src/api/types.ts
+++ src/api/types.ts
@@ -36,7 +36,7 @@ export type AddressCreate = {
 export type UserProfileBase = {
   firstname: string
   lastname: string
-  phoneNumber: string
+  phoneNumber?: string
 }
 
 export type UserProfile = UserProfileBase & {
@@ -93,7 +93,7 @@ export type ApiSellerContact = {
   // firstname: string
   // lastname: string
   email: string
-  phoneNumber: string
+  phoneNumber?: string
 }
 
 export type ApiSellerBase = {
