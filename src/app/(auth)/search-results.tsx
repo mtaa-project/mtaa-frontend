b51c140af99d5473cc041a8f78b9076678c78580
samuel-racak <samuel.racak@gmail.com>
2025-05-04 22:07:54 +0200

    Add SearchResultsScreen component: implement search results display using local search parameters

---
+++ src/app/(auth)/search-results.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { useLocalSearchParams } from "expo-router"
+import { SearchResults } from "@/src/features/search-results/search-results-screen"
+
+export default function SearchResultsScreen() {
+  const { query } = useLocalSearchParams<{ query: string }>()
+
+  return <SearchResults query={query ?? ""} />
+}
