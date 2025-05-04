c51705d9030bb5c363acea2bad5a6dde20364e68
samuel-racak <samuel.racak@gmail.com>
2025-05-04 22:07:48 +0200

    Refactor FavoriteScreen component: remove unused auth import and streamline return statement

--- src/app/(auth)/(tabs)/favorites.tsx
+++ src/app/(auth)/(tabs)/favorites.tsx
@@ -1,11 +1,7 @@
-import { auth } from "@/firebase-config"
 import { FavoriteListings } from "@/src/features/favorites/favorite-screen"
 
 export default function FavoriteScreen() {
-    const user = auth.currentUser
-    
-    return (
-        <FavoriteListings/> // This is a placeholder for the actual component
-    )
-  }
-  
+  return (
+    <FavoriteListings /> // This is a placeholder for the actual component
+  )
+}
