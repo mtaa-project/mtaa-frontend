7a5aa9ec9c24ab60a010fc8b2a846e0f9fc00c1f
samuel-racak <samuel.racak@gmail.com>
2025-05-05 21:58:50 +0200

    Add CategoryPill component: implement button for category selection with icon support

---
+++ src/features/home/components/category-pill.tsx
@@ -0,0 +1,65 @@
+import React from "react"
+import { StyleSheet, View } from "react-native"
+import { Button, Text, TouchableRipple, useTheme } from "react-native-paper"
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons"
+import { useGlobalStyles } from "@/src/components/global-styles"
+
+export interface Category {
+  id: number
+  name: string
+  iconName?: keyof typeof MaterialCommunityIcons.glyphMap // fallback handled below
+}
+
+interface Props {
+  category: Category
+  onPress?: () => void
+}
+
+export const CategoryPill: React.FC<Props> = ({ category, onPress }) => {
+  const theme = useTheme()
+
+  return (
+    // <TouchableRipple
+    //   onPress={onPress}
+    //   borderless
+    //   style={globalStyles.pageContainer}
+    // >
+    //   <View style={styles.content}>
+    //     <MaterialCommunityIcons
+    //       name={category.iconName ?? "bike"} // fallback icon because backend doesn't provide one
+    //       size={24}
+    //     />
+    //     <Text style={styles.label}>{category.name}</Text>
+    //   </View>
+    //   </TouchableRipple>
+
+    <Button
+      style={styles.content}
+      icon={({ color, size }) => (
+        <MaterialCommunityIcons
+          name={category.iconName ?? "bike"}
+          size={size}
+          color={color}
+        />
+      )}
+      mode="contained"
+      onPress={onPress}
+    >
+      {category.name}
+    </Button>
+  )
+}
+
+const styles = StyleSheet.create({
+  content: {
+    alignSelf: "stretch",
+    gap: 8,
+    flexDirection: "row",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+  label: {
+    marginLeft: 8,
+    fontWeight: "500",
+  },
+})
